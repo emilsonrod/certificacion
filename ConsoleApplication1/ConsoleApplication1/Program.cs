fe903bd9946956306b3eb292f6e3576f8cd23e14
emilson <emilsonrod@gmail.com>
2019-05-15 20:14:00 -0400

    fisrt sample second class

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = Convert.ToInt32( Console.ReadLine());
+            int invertido = 0;
+            int unidad = numero % 10;
+            numero = numero / 10;
+            invertido = invertido * 10 + unidad;
+            int decena = numero % 10;
+            numero = numero / 10;
+            invertido = invertido * 10 + decena;
+            int centena = numero % 10;
+            numero = numero / 10;
+            invertido = invertido * 10 + centena;
+            Console.WriteLine("La suma es {0}", (unidad + decena + centena));
+            Console.WriteLine("El invertido es {0}", invertido); 
+        }
+    }
+}
